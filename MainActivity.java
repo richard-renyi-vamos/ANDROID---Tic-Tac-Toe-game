2e2a94bb6a5c95932aa322de43b66a62509f28a9
Richard Renyi-Vamos <82152266+richard-renyi-vamos@users.noreply.github.com>
2023-10-28 11:31:08 +0200

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,95 @@
+// MainActivity.java
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button[][] buttons = new Button[3][3];
+    private boolean playerX = true; // Player X starts
+    private int roundCount = 0;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.text_view_result);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                String buttonID = "button_" + i + j;
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                buttons[i][j] = findViewById(resID);
+                buttons[i][j].setOnClickListener(this);
+            }
+        }
+
+        Button resetButton = findViewById(R.id.button_reset);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetGame();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (!((Button) v).getText().toString().equals("")) {
+            return; // The button is already used
+        }
+
+        if (playerX) {
+            ((Button) v).setText("X");
+        } else {
+            ((Button) v).setText("O");
+        }
+
+        roundCount++;
+
+        if (checkForWin()) {
+            if (playerX) {
+                updateResult("Player X wins!");
+            } else {
+                updateResult("Player O wins!");
+            }
+        } else if (roundCount == 9) {
+            updateResult("It's a draw!");
+        } else {
+            playerX = !playerX;
+        }
+    }
+
+    private boolean checkForWin() {
+        // Check rows, columns, and diagonals for a win
+        // Implement your winning condition logic here
+
+        return false;
+    }
+
+    private void updateResult(String result) {
+        textView.setText(result);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setClickable(false);
+            }
+        }
+    }
+
+    private void resetGame() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+                buttons[i][j].setClickable(true);
+            }
+        }
+        playerX = true;
+        roundCount = 0;
+        textView.setText("");
+    }
+}
